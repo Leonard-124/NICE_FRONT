349124404001503bf3acfe4397c1d38a1e004ea3
Leonard-124 <leonardoduor91@gmail.com>
2026-02-05 13:11:34 +0300

    URL mishap changes

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -11,14 +11,14 @@ export const Images = [
             image: "https://images.pexels.com/photos/14019734/pexels-photo-14019734.jpeg",
             description: "Is Your Sensitive Data Truly Secure",
             author: "L.O",
-            link: "http://localhost:5173/card1" 
+            link: "https://ibonnis.com/card1" 
         },
         {
             id: 2,
             image: "https://images.pexels.com/photos/7988086/pexels-photo-7988086.jpeg",
             description: "Will AI Kill Pair Programming?",
             author: "L.O",
-            link: "http://localhost:5173/card2"
+            link: "https://ibonnis.com/card2"
         },
         {
 
@@ -26,20 +26,20 @@ export const Images = [
             image: "https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg",
             description: "The Difference Between UX and UI Design: A Beginner’s Guide",
             author: "L.O",
-            link: "http://localhost:5173/card3"
+            link: "https://ibonnis.com/card3"
         },
         {
             id: 4,
             image: learn,
             description: "Which Tech Career Path Is Right for You? 6 of the Best Tech Jobs Right Now",
             author: "L.O.",
-            link: "http://localhost:5173/card4"
+            link: "https://ibonnis.com/card4"
         },
         {
             id: 5,
     image: "https://miro.medium.com/v2/resize:fit:720/format:webp/0*87cD7-YKwcl_Quvu",
             description: "From Facts & Metrics to Media Machine Learning: Evolving the Data Engineering Function at Netflix",
             author: "L.O",
-            link: "http://localhost:5173/card5"
+            link: "https://ibonnis.com/card5"
         },
     ]
